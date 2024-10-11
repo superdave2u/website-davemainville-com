1fd733f38895e43bd76e87035f135c3750ee4396
Dave Mainville <dave@davemainville.com>
2024-10-11 13:46:11 -0400

    cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dave Mainville';
+export const SITE_DESCRIPTION = 'Full-stack developer, tech enthusiast, and adventurer sharing insights on technology, and personal growth.';
